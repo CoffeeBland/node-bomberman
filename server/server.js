bbadec90c8d17af9369edc4d9a21a4489667a675
Frederic Gingras <frederic@gingras.cc>
2014-01-26 12:34:30 -0500

    Client files organization & ready for Heroku

--- server/server.js
+++ server/server.js
@@ -2,21 +2,22 @@ CLIENT_SERVER_PORT = 8080;
 SOCKET_SERVER_PORT = 1337;
 var express = require('express')
   , app = express()
-  , fs = require('fs');
+  , fs = require('fs')
+  , engines = require('consolidate');
 
 // Web server part
 app.configure(function(){
-  app.engine('html', require('consolidate').hogan);
+  app.engine('html', engines.hogan);
 
-  app.use(express.static(__dirname + '/../client'));
+  app.use(express.static(__dirname + '/../client/public'));
   app.use(express.static(__dirname + '/../shared'));
 
-  app.set("views", __dirname + "/../client/")
+  app.set("views", __dirname + "/../client/views")
 });
 
 // Routes
 app.get('/', function(req, res){
-  res.render("index.html", {host: 'http://' + req.headers.host + ':' + SOCKET_SERVER_PORT});
+  res.render("index.html", {host: 'http://' + req.headers.host.replace(CLIENT_SERVER_PORT, SOCKET_SERVER_PORT)});
 });
 
 app.listen(CLIENT_SERVER_PORT);
